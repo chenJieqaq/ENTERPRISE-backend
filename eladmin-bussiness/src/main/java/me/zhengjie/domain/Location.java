08c43f2af25fd9b1230728a02a87e40e8b7e9027
ph <mopengdeng@gmail.com>
2023-04-20 01:22:37 +0800

    将前端接收的经纬度，调用百度地图api转换为城市（location）

---
+++ eladmin-bussiness/src/main/java/me/zhengjie/domain/Location.java
@@ -0,0 +1,10 @@
+package me.zhengjie.domain;
+
+
+import lombok.Data;
+
+@Data
+public class Location {
+    private double lng;
+    private double lat;
+}
---
+++ eladmin-bussiness/src/main/java/me/zhengjie/rest/LocationController.java
@@ -0,0 +1,32 @@
+package me.zhengjie.rest;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.RequiredArgsConstructor;
+import me.zhengjie.domain.Location;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/location")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class LocationController {
+
+    @PostMapping
+//    @PreAuthorize("admin")
+    public String getLocation(@RequestBody Location location) {
+        String url = "http://api.map.baidu.com/reverse_geocoding/v3/?ak={ak}&output=json&coordtype=wgs84ll&location={lat},{lng}";
+        String ak = "NRLkmp5ELkuF2gpi5qNc52HLtGAwbeBt"; // 请替换为自己的百度地图API的ak
+        double lat = location.getLat();
+        double lng = location.getLng();
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class, ak, lat, lng);
+
+        JSONObject jsonObject = JSONObject.parseObject(response);
+        JSONObject addressComponent = jsonObject.getJSONObject("result").getJSONObject("addressComponent");
+        String city = addressComponent.getString("city");
+
+        return city;
+    }
+}
